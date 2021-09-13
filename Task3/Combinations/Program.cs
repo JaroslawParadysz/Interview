683de764a41235b08d6d0cce23b275926712b52b
Jaroslaw Paradysz <jaroslaw.a.paradysz@capgemini.com>
2021-09-13 07:23:48 +0200

    Added Task 3

---
+++ Task3/Combinations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution();
+        }
+
+        private static void Solution()
+        {
+            string[] letters = new string[] { "a", "b", "c", "d" };
+
+            //TODO: implement a solution that displays two-element combinations(the order does not metter) from the letters array
+            //Example: "ab", "bc", "cd"
+        }
+    }
+}
